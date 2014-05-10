1b3f98b2f6dd67e138eb0822ed1e43e31a1da343
Matt Saucier <saucim@gmail.com>
2014-05-10 13:21:28 -0400

    Changed timing related to shifting in to neutral, included debouncing changes. Changed E-prom write address such that variables are now stored near the end of the E-prom. Header File should be adjusted to account for the E-prom not being the one which normally goes with this microcontroller.

--- shifting_test.c
+++ shifting_test.c
@@ -1,4 +1,3 @@
-//Test commit
 #include "simpletools.h"
 
 #define UP_SHIFT_PADDLE 12
@@ -8,42 +7,87 @@
 #define UP_SHIFT_AIR 3
 #define DOWN_SHIFT_AIR 4
 
-#define CLUTCH_DELAY 200
-#define SHIFT_DELAY 200
+#define CLUTCH_ENGAUGE_DELAY 70
+#define CLUTCH_RELEASE_DELAY 20
+#define SHIFT_DELAY 165
+#define NEUTRAL_FIND 32
+
 #define DEBOUNCE_TIME 13
 
-void Port_Init(void); //Port initializations
-void Shift(char paddle);
+#define EEPROM_ADDRESS 0xFFEE
 
-void main() {
+void ShiftCheck( char paddle );
+void Shift( char paddle );
+
+int main() {
 	char gear = 0;
-	Port_Init();
-	while(1) {
-		if (input(UP_SHIFT_PADDLE)) {
-			Shift(UP_SHIFT_AIR);
-			gear++;
+	int shifts = ee_getInt(EEPROM_ADDRESS);
+	
+	print( "Shifts: %d\n", ee_getInt( EEPROM_ADDRESS ) );
+	pause( 1000 );
+	while( 1 ){
+		if ( input( UP_SHIFT_PADDLE ) ) {
+			ShiftCheck( UP_SHIFT_PADDLE );
+			while (input(UP_SHIFT_PADDLE))
+       {
+         ;
+       }
+			pause(30);
+			shifts++;
+			ee_putInt( shifts, EEPROM_ADDRESS );
+    		gear++;
 		}
-		if (input(DOWN_SHIFT_PADDLE)) {
-			Shift(DOWN_SHIFT_AIR);
+
+		if ( input( DOWN_SHIFT_PADDLE ) ) {
+			ShiftCheck( DOWN_SHIFT_PADDLE );
+			while (input(DOWN_SHIFT_PADDLE)){
+          if (input(UP_SHIFT_PADDLE)){
+              pause (DEBOUNCE_TIME);
+                if(input(UP_SHIFT_PADDLE)){
+          
+                high(UP_SHIFT_AIR);
+                pause(NEUTRAL_FIND);
+                low(UP_SHIFT_AIR);
+                while(input(UP_SHIFT_PADDLE));
+              }  
+          }
+      }
+			pause(30);
+			shifts++;
+			ee_putInt( shifts, EEPROM_ADDRESS );
 			gear--;
 		}
+	}
+	return 0;
+}
 
+void ShiftCheck( char paddle ) {
+	int goodpress;
+	goodpress = 1;
+	pause( 40 );
+   if( !input( paddle ) ) {
+      goodpress = 0;
+   pause( 30 );
+   
+	}
+	if( goodpress == 1 ) {
+     if(paddle == UP_SHIFT_PADDLE){
+        Shift(UP_SHIFT_AIR);
+    }
+    if(paddle == DOWN_SHIFT_PADDLE){
+    
+        Shift(DOWN_SHIFT_AIR);
+    }
+		goodpress = 0;
 	}
 }
 
-void Port_Init(void) {
-	DIRA |= 0x0004003E;
-	DIRA &= 0xFFFEFFFF;
-}
-
-void Shift(char paddle) {
-	high(ENGAGE_CLUTCH);
-	pause(CLUTCH_DELAY);
-	high(paddle);
-	pause(SHIFT_DELAY);
-	low(paddle);
-	pause(CLUTCH_DELAY);
-	low(ENGAGE_CLUTCH);
-	while(input(paddle));
-	pause(DEBOUNCE_TIME);
-}
+void Shift( char paddle ) {
+	high( ENGAGE_CLUTCH );
+	pause( CLUTCH_ENGAUGE_DELAY );
+	high( paddle );
+	pause( SHIFT_DELAY );
+	low( paddle );
+	pause( CLUTCH_RELEASE_DELAY );
+	low( ENGAGE_CLUTCH );
+} 
